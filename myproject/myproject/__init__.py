b7209a94a81fb1d7b4b3965196963bb7fccf07a6
aayoos <87476210+aayoos@users.noreply.github.com>
2023-05-29 13:41:28 +0530

    Project

---
+++ myproject/myproject/__init__.py
@@ -0,0 +1,5 @@
+#creating virtual env- mkvirtualenv (name)
+#activating existing venv- workon (name)
+#creating a proj- django-admin startproject (name)
+#creating app using manage.py file- python manage.py startapp (name)
+#To run our project -python manage.py runserver
